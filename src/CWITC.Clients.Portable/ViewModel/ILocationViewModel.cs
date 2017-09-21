b33c535c21bd6ccd27c2bfe4889bed4dca62216a
Drew Frisk <drew@drewfrisk.com>
2017-09-20 23:04:47 -0500

    added a map to display lunch locations (#60)
    * added a map to the lunch locations page
    
    can swap between list & map
    
    * tweaked some text
    
    * added permissions to manifest, they weren’t sticking from the assembly info

---
+++ src/CWITC.Clients.Portable/ViewModel/ILocationViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace CWITC.Clients.Portable
+{
+	public interface ILocationViewModel
+	{
+		string Title { get; set; }
+		string Description { get; }
+		double Latitude { get; }
+		double Longitude { get; }
+		ICommand Command { get; }
+	}
+}
--- src/CWITC.Clients.UI/Pages/Lunch/LunchLocationsPage.xaml.cs
+++ src/CWITC.Clients.UI/Pages/Lunch/LunchLocationsPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using CWITC.Clients.Portable;
 using CWITC.DataObjects;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 
 namespace CWITC.Clients.UI
 {
@@ -15,23 +16,62 @@ namespace CWITC.Clients.UI
         {
             InitializeComponent();
 
+            this.Map.MoveToRegion(MapSpan.FromCenterAndRadius(
+                new Position(44.523411, -89.583897),
+                Distance.FromMiles(0.25)));
+            
+            ToolbarItem showList = null;
+            ToolbarItem showMap = null;
+
+            showList = new ToolbarItem("Show List", "ic_view_list.png", () => 
+            {
+                ListViewLocations.IsVisible = true;
+                MapWrapper.IsVisible = false; 
+
+                ToolbarItems.Remove(showList);
+                ToolbarItems.Add(showMap);
+            });
+
+			showMap = new ToolbarItem("Show Map", "ic_map.png", () =>
+			{
+                ListViewLocations.IsVisible = false;
+				MapWrapper.IsVisible = true;
+
+                ToolbarItems.Remove(showMap);
+                ToolbarItems.Add(showList);
+			});
+
+            ToolbarItems.Add(showMap);
+
             BindingContext = new LunchLocationsViewModel(this.Navigation);
 
 			ListViewLocations.ItemTapped += (sender, e) => ListViewLocations.SelectedItem = null;
-			ListViewLocations.ItemSelected += async (sender, e) =>
+			ListViewLocations.ItemSelected += (sender, e) =>
 			{
 				var ev = ListViewLocations.SelectedItem as LunchLocation;
 				if (ev == null)
 					return;
 
-                var eventDetails = new LunchLocationDetailsPage();
-
-				eventDetails.Location = ev;
-                App.Logger.TrackPage(AppPage.LunchLocation.ToString(), ev.Name);
-				await NavigationService.PushAsync(Navigation, eventDetails);
+                ShowDetail(ev);
 
 				ListViewLocations.SelectedItem = null;
 			};
+
+            MapBehavior.ShowDetailCommand = new Command<ILocationViewModel>((x) =>
+            {
+                var ev = (x as LunchLocationsViewModel.LunchLocationPinViewModel);
+
+                ShowDetail(ev.Location);
+            });
+        }
+
+        async void ShowDetail(LunchLocation ev)
+        {
+			var eventDetails = new LunchLocationDetailsPage();
+
+			eventDetails.Location = ev;
+			App.Logger.TrackPage(AppPage.LunchLocation.ToString(), ev.Name);
+			await NavigationService.PushAsync(Navigation, eventDetails);
         }
 
 		protected override void OnAppearing()
